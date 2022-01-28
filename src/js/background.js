1f068084005652d25c7cbd577afe86b789c640d7
dimspith <51262483+dimspith@users.noreply.github.com>
2022-01-28 14:28:05 +0200

    Make extension block pages correctly

--- src/js/background.js
+++ src/js/background.js
@@ -1,33 +1,62 @@
 'use strict';
 
-// Get the background page. used for console logging
-// const bg = chrome.extension.getBackgroundPage();
+
+const isProtectedOrEmpty = (url) => {
+    const protectedPrefixes = [
+        "chrome://",
+        "brave://",
+        "chrome-extension://"
+    ];
+    if (!url || url.length === 0) {
+        return true;
+    }
+    for(var i = 0; i < protectedPrefixes.length; i++) {
+        if(url.startsWith(protectedPrefixes[i])) {
+            console.log("Url is protected! Starts with: " + protectedPrefixes[i]);
+            return true;
+        }
+    }
+    return false;
+};
 
 // Function ran when a site is blocked. By default it redirects to a custom page.
 // Don't block the current domain is in the whitelist
-const blockRequest = (request) => {
+const blockIfFake = (url, tabID) => {
     chrome.storage.local.get(['enabled', 'whitelist'], data => {
         if(data.enabled) {
-            chrome.tabs.query({currentWindow: true, active: true}, function (tab) {
-                const domain = new URL(request.url).hostname.replace('www.','');
-                if(!data.hasOwnProperty('whitelist') ||!data.whitelist.includes(domain)) {
-                    let blocked = chrome.extension.getURL("src/blocked.html")
-                        .concat(`?blocked-page=${request.url}`);
-                    chrome.tabs.update(tab.id, {url: blocked});             
-                }
-            });
+            const domain = url.replace('www.','');
+            if(isProtectedOrEmpty(url) || (data.hasOwnProperty('whitelist') && data.whitelist.includes(domain))) {
+                console.log(domain + " in whitelist or protected!");
+            } else {
+                console.log(domain + " not whitelisted!");
+                let blocked = chrome.runtime.getURL("src/blocked.html")
+                    .concat(`?blocked-page=${url}`);
+                chrome.tabs.update(tabID, {url: blocked});
+            }
+        } else {
+            console.log("Extension not enabled!");
         }
     });
 };
 
-// Fetch the url list from localstorage and update the listener
-function updateListener() {
-    // TODO
-};
+chrome.tabs.onActivated.addListener( function(activeInfo){
+    chrome.tabs.get(activeInfo.tabId, function(tab){
+        const url = tab.url;
+        console.log("(Tab activated) you are here: " + url);
+        blockIfFake(url, tab.id);
+    });
+});
+
+chrome.tabs.onUpdated.addListener((tabId, change, tab) => {
+    if (tab.active && change.url) {
+        const url = change.url;
+        console.log("(Tab updated) you are here: " + url);
+        blockIfFake(url, tab.id);
+    }
+});
 
 // Wait for messages from other pages withing the extension
 chrome.runtime.onMessage.addListener((request) => {
-
     // Message requesting a blacklist update
     if (request.message == "update") {
         updateListener();
