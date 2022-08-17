183f73f86e37011d33d484a573afdda8f17d7775
dimspith <51262483+dimspith@users.noreply.github.com>
2022-08-18 00:03:10 +0300

    Hide extra labelling tags when the domain is not labelled as "fake news"

--- src/js/labelling.js
+++ src/js/labelling.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // Configure Toast
 bulmaToast.setDefaults({
   duration: 3000,
@@ -52,8 +54,26 @@ u('.cancel').on("click", () => {
     window.close();
 });
 
+// Hide/Show extra fields depending on the fake news status of the domain
+function check() {
+    if (document.getElementById("is-not-fake").checked) {
+        u(".fake-news-data").addClass('is-hidden');
+    } else if (document.getElementById("is-fake").checked) {
+        u(".fake-news-data").removeClass('is-hidden');
+    }
+}
+
+addEventListener("change", ({target}) => {
+    if(target.matches("input[type='radio']")) {
+        check();
+    }
+});
+
+// Get domain from URL parameters
 const urlParams = new URLSearchParams(window.location.search);
 if(urlParams.has('domain')) {
     const domain = urlParams.get('domain');
     u('.domain').attr('value', domain);
 }
+
+
